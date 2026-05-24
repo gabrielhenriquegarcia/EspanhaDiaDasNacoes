225da3551529371ff38244602ea1de5e1e951a43
gabrielhenriquegarcia <gabrielhenrique.garcia1@gmail.com>
2026-05-24 12:58:30 -0300

    Delete indexscript.js

--- indexscript.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".split-side").forEach((side) => {
-  side.addEventListener("mouseenter", () => {
-    side.classList.add("semfiltro");
-  });
-
-  side.addEventListener("mouseleave", () => {
-    side.classList.remove("semfiltro");
-  });
-});
